33a797929993e286822f0ae1813067eadd463c81
tfunato <tfunato@healthcare.ne.jp>
2014-10-07 18:56:27 +0900

    add other properties set.

--- src/main/java/jp/canetrash/ingress/mail/SimpleParser.java
+++ src/main/java/jp/canetrash/ingress/mail/SimpleParser.java
@@ -32,15 +32,58 @@ public class SimpleParser extends AbstractParser {
 				final String contentType = part.getContentType();
 				if (contentType.indexOf("text/html") != -1) {
 					Document doc = Jsoup.parse(part.getContent().toString());
+					// agent name
+					Elements element = doc.select(AGENT_NAME_CSSSELECTOR);
+					mail.setAgentName(element.text());
+					// faction
+					element = doc.select(FACTION_CSSSELECTOR);
+					mail.setFaction(element.text());
+					// agent level
+					element = doc.select(AGENT_LEVEL_CSSSELECTOR);
+					mail.setAgentLevel(element.text());
 					// portal name
-					Elements element = doc
-							.select("body > div > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(3) > td > div:nth-child(1)");
+					element = doc.select(PORTAL_NAME_CSSSELECTOR);
 					mail.setPortalName(element.text());
-
+					// portal address
+					element = doc.select(PORTAL_ADDRESS_CSSSELECTOR);
+					mail.setPortalAddress(element.text());
+					// portal url
+					String url = element.attr("href");
+					mail.setPortalIntelUrl(url);
+					// portal image url
+					element = doc.select(PORTAL_IMAGE_URL_CSSSELECTOR);
+					mail.setPortalImageUrl(element.attr("src"));
+					// portal status image url
+					element = doc.select(PORTAL_STATUS_IMAGE_URL_CSSSELECTOR);
+					mail.setPortalStatusImageUrl(element.attr("src"));
+					// enemy agent name
+					element = doc.select(ENEMY_AGENT_NAME_CSSSELECTOR);
+					mail.setEnemyAgentName(element.text());
+					element = doc.select(CURRENT_PORTAL_STATUS_CSSSELECTOR);
+					String status = element.text();
+					// status format
+					// STATUS: Level 1 Health: 0% Owner: [uncaptured]
+					String[] strArry = status.split(" ");
+					// current portal level
+					mail.setCurrentPortalLevel(strArry[2]);
+					// current portal health
+					mail.setCurrentPortalHealth(strArry[4]);
+					// current portal owner
+					mail.setCurrentPortalOwner(strArry[6]);
 				}
 			}
 		}
 		return mail;
 	}
+
+	private static final String PORTAL_NAME_CSSSELECTOR = "body > div > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(3) > td > div:nth-child(1)";
+	private static final String AGENT_NAME_CSSSELECTOR = "body > div > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td > span:nth-child(2)";
+	private static final String FACTION_CSSSELECTOR = "body > div > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td > span:nth-child(5)";
+	private static final String AGENT_LEVEL_CSSSELECTOR = "body > div > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td > span:nth-child(8)";
+	private static final String PORTAL_ADDRESS_CSSSELECTOR = "body > div > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(3) > td > div:nth-child(2) > a";
+	private static final String PORTAL_IMAGE_URL_CSSSELECTOR = "body > div > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(4) > td > div > div:nth-child(1) > img";
+	private static final String PORTAL_STATUS_IMAGE_URL_CSSSELECTOR = "body > div > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(4) > td > div > div:nth-child(2) > img";
+	private static final String ENEMY_AGENT_NAME_CSSSELECTOR = "body > div > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(5) > td > table > tbody > tr > td:nth-child(1) > div > span";
+	private static final String CURRENT_PORTAL_STATUS_CSSSELECTOR = "body > div > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(5) > td > table > tbody > tr > td:nth-child(2) > div";
 
 }
--- src/test/java/jp/canetrash/ingress/mail/SimpleParserTest.java
+++ src/test/java/jp/canetrash/ingress/mail/SimpleParserTest.java
@@ -24,7 +24,24 @@ public class SimpleParserTest {
 		assertThat(actual.getSubject(),
 				is("Ingress Damage Report: Entities attacked by matahaari"));
 		assertThat(actual.getPortalName(), is("恵比寿駅前噴水"));
-
+		assertThat(actual.getAgentName(), is("atomosphere"));
+		assertThat(actual.getFaction(), is("Resistance"));
+		assertThat(actual.getAgentLevel(), is("L9"));
+		assertThat(
+				actual.getPortalAddress(),
+				is("Komazawa Dori, １丁目 Ebisuminami, Shibuya, Tokyo 150-0013, Japan"));
+		assertThat(
+				actual.getPortalIntelUrl(),
+				is("https://www.ingress.com/intel?ll=35.647189,139.709069&pll=35.647189,139.709069&z=19"));
+		assertThat(
+				actual.getPortalImageUrl(),
+				is("http://lh6.ggpht.com/FUZkUTk6re3dn9QKX3odoai2rb6HEbVt3mV5uZKON8DkZEA0L7BuVXLrqDC7smv9mcY7nsrwCl9BXD61B-k"));
+		assertThat(
+				actual.getPortalStatusImageUrl(),
+				is("http://maps.googleapis.com/maps/api/staticmap?center=35.647189,139.709169&zoom=19&size=700x160&sensor=false&style=visibility:on%7Csaturation:-50%7Cinvert_lightness:true%7Chue:0x131c1c&style=feature:water%7Cvisibility:on%7Chue:0x005eff%7Cinvert_lightness:true&style=feature:poi%7Cvisibility:off&style=feature:transit%7Cvisibility:off&markers=icon:http://commondatastorage.googleapis.com/ingress.com/img/map_icons/marker_images/neutral_icon.png%7Cshadow:false%7C35.647189,139.709069"));
+		assertThat(actual.getEnemyAgentName(), is("matahaari"));
+		assertThat(actual.getCurrentPortalLevel(), is("1"));
+		assertThat(actual.getCurrentPortalHealth(), is("0%"));
+		assertThat(actual.getCurrentPortalOwner(), is("[uncaptured]"));
 	}
-
 }
